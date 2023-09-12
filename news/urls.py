7de380b12b85623a317b71a1e9c4fff09b19fb22
Marcos Adriano <marcosadriano.ti@gmail.com>
2023-09-12 09:00:33 -0300

    Req 05

--- news/urls.py
+++ news/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from news.views import home
+from news.views import home, news_details_view
 
 urlpatterns = [
     path("", home, name="home-page"),
+    path("<int:id>/", news_details_view, name="news-details-page"),
 ]
--- news/views.py
+++ news/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import News
 
 
 def home(request):
-    # news_list = News.objects.all()
     context = {"all_news": News.objects.all()}
     return render(request, 'home.html', context)
+
+
+def news_details_view(request, id):
+    context = {"news": get_object_or_404(News, id=id)}
+    return render(request, 'news_details.html', context)
